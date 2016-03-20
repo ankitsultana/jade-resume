bfcb019c3581e7c136e090cbd1e124bfc7acf3c2
bk2dcradle <ankitsultana@gmail.com>
2016-03-20 21:49:11 +0530

    update

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            jade: {
+                files: ['index.jade'],
+                tasks: ['jade']
+            },
+            sass: {
+                files: ['includes/sass/style.scss'],
+                tasks: ['sass']
+            }
+        },
+        jade: {
+            compile: {
+                files: {
+                    'index.html': 'index.jade'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'includes/css/style.css': 'includes/sass/style.scss'
+                }
+            }
+        }
+    })
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-contrib-jade')
+    grunt.loadNpmTasks('grunt-contrib-sass')
+}
